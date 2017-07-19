44b4ef0ca744ded2873c47f8c785863b04d6aa8e
Nik Barham <nick.barham@musicgateway.net>
2017-07-19 11:45:04 +0000

    Incorrect http status code value

--- src/Exception/MethodNotAllowed.php
+++ src/Exception/MethodNotAllowed.php
@@ -6,7 +6,7 @@ class MethodNotAllowed extends Exception
 {
     public function __construct(array $allowed, $message, $context = null, \Throwable $previous = null)
     {
-        parent::__construct($message, $context, $previous, 404);
+        parent::__construct($message, $context, $previous, 405);
         $this->setHeaders('Allow', strtoupper(implode(', ', $allowed)));
     }
 }
